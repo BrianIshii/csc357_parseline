294ee5023c6a822c49ada27d4ff23dd766b2a619
brianishii <brianlishii@gmail.com>
2018-03-05 16:12:00 -0800

    added function headers

--- parseline.h
+++ parseline.h
@@ -6,3 +6,26 @@
 
 
 #include<stdio.h>
+
+#define CMD_LINE_MAX 1024
+#define CMD_PIPE_MAX 20
+#define CMD_ARGS_MAX 20
+
+typedef struct command Command;
+
+struct command {
+    char commandline[CMD_LINE_MAX];
+    char *input;
+    char *output;
+    int argc;
+    char *argv[CMD_ARGS_MAX];
+}
+
+/* reads in args and returns a command struct */
+Command *parseCommand(char *line);
+
+/* print commands */
+void printCommand(int stageNum, Command *command);
+
+/* initialize struct command */
+void initializeCommand(Command *command);
